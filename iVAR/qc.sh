3495a5bedc194aa0f3faa090c84b9b609e618ab8
itrus <itrus@users.noreply.github.com>
2022-02-17 10:09:47 +0100

    Add files via upload

---
+++ iVAR/qc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SEARCH_PATTERN='*.fastq.gz'
+
+TOTAL_FILES=`find ./ -maxdepth 1 -iname "*$SEARCH_PATTERN*" | wc -l`
+ARR=($(ls $SEARCH_PATTERN))
+echo "[running QC]"
+ls $SEARCH_PATTERN | parallel -j 4 --progress ~/bin/FastQC/fastqc {} 
+printf "\n"
+for ((i=0; i<$TOTAL_FILES; i+=1)) {
+    FILE_NAME=`echo ${ARR[$i]} | awk -F "." '{print $1}'`    
+    echo "[processing] $FILE_NAME"
+    printf "Reads, thousands: "
+    zcat ${ARR[$i]} | echo $((`wc -l`/4/1000)) 
+} 
+printf "\n"
+printf "Total reads, thousands: "
+zcat $SEARCH_PATTERN | echo $((`wc -l`/4/1000))
