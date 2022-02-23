950919730a06ef3dcc05b4abee516eda1d24ba22
itrus <itrus@users.noreply.github.com>
2022-02-23 14:01:51 +0100

    Update index.sh

--- RNA-seq/index.sh
+++ RNA-seq/index.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-date
-INDEX=homo105.idx
+
+# filenames for input and output files
 CDNA=Homo_sapiens.GRCh38.cdna.all.fa.gz
 GTF=Homo_sapiens.GRCh38.105.gtf.gz
+INDEX=homo105.idx
+
+date
 KALLISTO=~/bin/kallisto
 $KALLISTO/kallisto index -i $INDEX $CDNA
 $KALLISTO/kallisto inspect -g $GTF $INDEX
